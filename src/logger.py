9df1ec793664eef0b6b7975cd38382acf2d21bc2
ADITYA2026-coder <adityatripathi16804@gmail.com>
2026-06-07 08:59:50 +0530

    logging and exception

---
+++ src/logger.py
@@ -0,0 +1,19 @@
+# logger is for the purpose that any execution that happens we should be able to log all those information in some files so that we will be able to track if there is some errors(even custom exception error).
+import logging
+import os
+from datetime import datetime
+
+LOG_FILE=f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+logs_path=os.path.join(os.getcwd(),"logs",LOG_FILE)
+os.makedirs(logs_path,exist_ok=True)
+
+LOG_FILE_PATH=os.path.join(logs_path,LOG_FILE)
+
+logging.basicConfig(
+    filename=LOG_FILE_PATH,
+    format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
+    level=logging.INFO,
+ )
+
+if __name__=="__main__":
+    logging.info("Logging has started")
